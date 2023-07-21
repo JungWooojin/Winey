8cd68411f8234940598990539e4c5c11e888a11c
김원희 <kwh1619@naver.com>
2023-07-21 17:08:53 +0900

    wh main 작성 중

--- src/main/java/com/team/winey/main/MainController.java
+++ src/main/java/com/team/winey/main/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/api/main")
 @RequiredArgsConstructor
 public class MainController {
 
