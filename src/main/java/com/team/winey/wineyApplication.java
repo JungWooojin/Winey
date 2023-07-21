acceac3cc9617fcc014279154ff93be7597dc4a3
정우진 <wash98@naver.com>
2023-07-21 14:52:25 +0900

    -start-

--- src/main/java/com/team/winey/wineyApplication.java
+++ src/main/java/com/team/winey/wineyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectDApplication {
+public class wineyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectDApplication.class, args);
+        SpringApplication.run(wineyApplication.class, args);
     }
 
 }
