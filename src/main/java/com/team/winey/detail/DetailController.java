4e9f0bf360d4f860e095c2ccbf550f7db227d451
배서윤 <qaz197@naver.com>
2023-08-07 14:53:57 +0900

    sy detail page

--- src/main/java/com/team/winey/detail/DetailController.java
+++ src/main/java/com/team/winey/detail/DetailController.java
@@ -1,14 +1,17 @@
 package com.team.winey.detail;
 
+import com.team.winey.detail.model.SelWineKorNm;
 import com.team.winey.detail.model.WineDetailVo;
 import com.team.winey.detail.model.WineVo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "와인상세페이지")
 @RestController
 @RequestMapping("/detail")
 @RequiredArgsConstructor
@@ -23,7 +26,11 @@ public class DetailController {
         return SERVICE.selWineDetail(productId);
 
 
+    }
 
-
+    @GetMapping("/korNm/{productId}")
+    @Operation(summary = "와인 한글이름")
+    public SelWineKorNm getWineKorNm(@PathVariable Long productId) {
+        return SERVICE.selKorNm(productId);
     }
 }
