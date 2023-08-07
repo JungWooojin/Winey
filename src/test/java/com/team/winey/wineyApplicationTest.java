db2e2caf700b7745ffe1a5beff6607a1f318a8d9
김원희 <kwh1619@naver.com>
2023-08-07 19:57:28 +0900

    wh tdd 에러

---
+++ src/test/java/com/team/winey/wineyApplicationTest.java
@@ -0,0 +1,15 @@
+package com.team.winey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class wineyApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
