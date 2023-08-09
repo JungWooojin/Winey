8812bf5ecc641b612b4bcb0430e4dafc30824fcd
김원희 <kwh1619@naver.com>
2023-08-09 14:53:56 +0900

    wh main 수정

---
+++ src/test/java/com/team/winey/main/MainControllerTest.java
@@ -0,0 +1,20 @@
+package com.team.winey.main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainControllerTest {
+
+    @Test
+    void getWines() {
+    }
+
+    @Test
+    void getWinebyId() {
+    }
+
+    @Test
+    void getWineByPrice2() {
+    }
+}
