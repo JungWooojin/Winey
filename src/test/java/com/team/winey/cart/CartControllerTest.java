4baaa89e9670ad2243537f52f03126a59ad2fc8d
Haminsu <dldlma@naver.com>
2023-08-14 13:15:45 +0900

    ms test

---
+++ src/test/java/com/team/winey/cart/CartControllerTest.java
@@ -0,0 +1,72 @@
+package com.team.winey.cart;
+
+import com.team.winey.cart.model.CartInsDto;
+import com.team.winey.cart.model.CartVo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(CartController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class CartControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CartService service;
+
+    @Test
+    void postCart() throws Exception {
+
+        given(service.insCart(any(CartInsDto.class))).willReturn(1);
+
+        CartInsDto dto = new CartInsDto();
+        dto.setQuantity(1);
+        dto.setProductId(1);
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/wine/cart")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"productId\": 1, \"quantity\": 2}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("1"));
+
+        verify(service).insCart(any());
+    }
+
+    @Test
+    void getFilledCart() throws Exception {
+        List<CartVo> mockList = new ArrayList<>();
+
+       given(service.selCart()).willReturn(mockList);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/wine/filledcart"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(mockList.size()));
+        verify(service).selCart();
+    }
+
+    @Test
+    void delCart() {
+    }
+
+    @Test
+    void putCart() {
+    }
+}
