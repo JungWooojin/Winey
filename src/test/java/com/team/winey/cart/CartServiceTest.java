420c7898b3ee4e00f5ee3b9d5e4056f9e256e34c
Haminsu <dldlma@naver.com>
2023-08-14 12:37:11 +0900

    ms test

--- src/test/java/com/team/winey/cart/CartServiceTest.java
+++ src/test/java/com/team/winey/cart/CartServiceTest.java
@@ -1,7 +1,6 @@
 package com.team.winey.cart;
 
-import com.team.winey.cart.model.CartInsDto;
-import com.team.winey.cart.model.CartInsDto2;
+import com.team.winey.cart.model.*;
 import com.team.winey.config.security.AuthenticationFacade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,9 +12,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -55,13 +57,40 @@ class CartServiceTest {
 
     @Test
     void selCart() {
+        CartSelDto dto = new CartSelDto();
+        dto.setUserId(facade.getLoginUserPk());
+        when(dto.getUserId()).thenReturn(1l);
+
+        List<CartVo> expectedCartList = new ArrayList<>();
+        when(mapper.selCart(any())).thenReturn(expectedCartList);
+
+        List<CartVo> cartList = service.selCart();
+
+        assertEquals(expectedCartList, cartList);
     }
 
     @Test
     void delCart() {
+        CartdelDto dto = new CartdelDto();
+        int expectResult = 1;
+        when(mapper.delCart(any(CartdelDto.class))).thenReturn(expectResult);
+
+        int result = service.delCart(dto);
+        assertEquals(expectResult, result);
+
+        verify(mapper).delCart(any(CartdelDto.class));
     }
 
     @Test
     void updCart() {
+        CartUpdDto dto = new CartUpdDto();
+        dto.setCartId(1);
+        dto.setQuantity(1);
+
+        when(mapper.updCart(dto)).thenReturn(1);
+        int result = service.updCart(dto);
+
+        assertEquals(1,result);
+        verify(mapper).updCart(any());
     }
 }
