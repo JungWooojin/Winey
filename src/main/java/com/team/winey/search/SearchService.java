bdaf02a91cf2851521b1cc1a76a9623ca7e90974
김원희 <kwh1619@naver.com>
2023-08-30 17:44:04 +0900

    wh search 수정

--- src/main/java/com/team/winey/search/SearchService.java
+++ src/main/java/com/team/winey/search/SearchService.java
@@ -25,6 +25,16 @@ public class SearchService {
         int maxPage = (int) Math.ceil((double) count / dto.getRow());
         int isMore = maxPage > dto.getPage() ? 1 : 0;
 
+        if(dto.getBigCategoryId() == null) {
+            return null;
+        }
+        if(dto.getCategoryId() == null) {
+            return null;
+        }
+        if(dto.getCountryId() == null) {
+            return null;
+        }
+
         return WineSelDetailRes.builder()
                 .categoryId(dto.getCategoryId())
                 .bigCategoryId(dto.getBigCategoryId())
