537425d02a3721ad47c0c8075798a40f6f330b05
김원희 <kwh1619@naver.com>
2023-08-30 16:05:49 +0900

    wh search수정

--- src/main/java/com/team/winey/search/SearchService.java
+++ src/main/java/com/team/winey/search/SearchService.java
@@ -37,7 +37,7 @@ public class SearchService {
                 .row(dto.getRow())
                 .isMore(isMore)
                 .maxPage(maxPage)
-                .count(list.size())
+                .count(count)
                 .wineList(list)
                 .build();
     }
