ec83dd4f5a230b4a1ccac0e25aad57e68406f02c
김원희 <kwh1619@naver.com>
2023-09-04 17:06:08 +0900

    wh /img/** 추가

--- src/main/java/com/team/winey/config/security/SecurityConfiguration.java
+++ src/main/java/com/team/winey/config/security/SecurityConfiguration.java
@@ -19,42 +19,44 @@ public class SecurityConfiguration {
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisTemplate<String, String> redisTemplate;
     private final RedisService redisService;
+
     //webSecurityCustomizer를 제외한 모든 것, 시큐리티를 거친다. 보안과 연관
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(authz ->
-                            authz.requestMatchers(
-                                             "/swagger.html"
-                                            , "/swagger-ui/**"
-                                            , "/*", "/admin/**"
-                                            , "/v3/api-docs/**"
-                                            , "/index.html"
-                                            , "/static/**"
-                                            , "/images/**"
-                                            , "/js/**"
-                                            , "/css/**"
+                        authz.requestMatchers(
+                                        "/swagger.html"
+                                        , "/swagger-ui/**"
+                                        , "/*", "/admin/**"
+                                        , "/v3/api-docs/**"
+                                        , "/index.html"
+                                        , "/static/**"
+                                        , "/images/**"
+                                        , "/img/**"
+                                        , "/js/**"
+                                        , "/css/**"
 
-                                            ,"/sign-api/sign-in"
-                                            , "/sign-api/sign-up"
-                                            , "/sign-api/exception"
+                                        , "/sign-api/sign-in"
+                                        , "/sign-api/sign-up"
+                                        , "/sign-api/exception"
 
-                                            , "/view/**"
-                                             ,"/api/**"
-                                    ).permitAll()
-                                    .requestMatchers(HttpMethod.GET, "/sign-api/refresh-token").permitAll()
-                                    .requestMatchers("**exception**").permitAll() //exception의  용도
-                                    .requestMatchers("/api/main").permitAll()// roles 값을 두개값으로 따로 check in 처럼 데이터에 제한을 두어야하는지 그냥 insert해야할지 ?
-                                    .requestMatchers("/api/recommend").hasAnyRole("USER","ADMIN")
-                                    .requestMatchers("/api/mypage").hasAnyRole("USER","ADMIN")
-                                    .requestMatchers("/api/wine").hasAnyRole("USER","ADMIN")
-                                    .requestMatchers("/api/admin").hasRole("ADMIN")
-                                    .requestMatchers("/api/download").hasRole("ADMIN")
-                                    .requestMatchers("/api/orderList").hasAnyRole("USER","ADMIN")
-                                    .requestMatchers("/api/detail").hasAnyRole("USER","ADMIN")
-                                    .requestMatchers("/api/payment").hasAnyRole("USER","ADMIN")
+                                        , "/view/**"
+                                        , "/api/**"
+                                ).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/sign-api/refresh-token").permitAll()
+                                .requestMatchers("**exception**").permitAll() //exception의  용도
+                                .requestMatchers("/api/main").permitAll()// roles 값을 두개값으로 따로 check in 처럼 데이터에 제한을 두어야하는지 그냥 insert해야할지 ?
+                                .requestMatchers("/api/recommend").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/mypage").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/wine").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/admin").hasRole("ADMIN")
+                                .requestMatchers("/api/download").hasRole("ADMIN")
+                                .requestMatchers("/api/orderList").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/detail").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/payment").hasAnyRole("USER", "ADMIN")
                 ) //사용 권한 체크
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 사용 X
-        .httpBasic(http -> http.disable()) //UI 있는 시큐리티 설정을 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 사용 X
+                .httpBasic(http -> http.disable()) //UI 있는 시큐리티 설정을 비활성화
                 .csrf(csrf -> csrf.disable()) //CSRF 보안이 필요 X, 쿠키와 세션을 이용해서 인증을 하고 있기 때문에 발생하는 일, https://kchanguk.tistory.com/197
                 .exceptionHandling(except -> {
                     except.accessDeniedHandler(new CustomAccessDeniedHandler());
